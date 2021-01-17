698e2a9b7e46869ae28f26fb611295648154d296
buseozkan <buse1.ozkan@gmail.com>
2021-01-17 21:46:47 +0200

    kata2

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,12 @@
+const { TestScheduler } = require("jest");
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
   // how do we create specs again???
+  
+  it("returns yes for true, no for false", () => {
+    expect(booleanToWord(true)).toBe('Yes')
+    expect(booleanToWord(false)).toBe('No')
+  })
+
 });
