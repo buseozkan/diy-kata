fce30d44c754db802434bdce0950f7af3ebe8864
buseozkan <buse1.ozkan@gmail.com>
2021-01-17 23:15:14 +0200

    kata

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,7 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    dogYears = 15 + 9 + (number - 2)*(5)
+    catYears = 15 + 9 + (number - 2)*(4)
+    return [number, catYears, dogYears]
+};
 
 module.exports = humanCatDogYears;
