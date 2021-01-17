1c371527d0613a8c262f97d94d4d0ba89cca8314
buseozkan <buse1.ozkan@gmail.com>
2021-01-17 23:15:53 +0200

    kata3

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,7 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+    let x = number.toString().split('').reverse()
+    let y = x.map(string => parseInt(string));
+    return y
+};
 
 module.exports = numberToReversedDigits;
